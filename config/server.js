1b1b28ec5fc8df0ac5fec5e4cc72b758039cde2b
aljubaer <bsse0812@iit.du.ac.bd>
2021-09-05 18:58:22 +0600

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ed27e06254cda95873ecd1e832d0883b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6adb3710-e660-4817-b081-d579dba3262e'
+};
